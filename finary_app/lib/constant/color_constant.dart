639bfbdc36b8dd268c350439750de2b59e0a81e8
naufalrizky79 <naufalrizky79@gmail.com>
2023-12-18 14:42:00 +0700

    Update color_constant.dart

---
+++ finary_app/lib/constant/color_constant.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  Color primaryBlue = const Color(0xff605DEC);
+}
+
+class StrokeColor {
+  Color focused = const Color(0xfff845fc);
+  Color grey = const Color(0xffbdbcdb);
+}
+
+class BackgroundColor {
+  Color white = const Color(0xffffffff);
+  Color whiteDisabled = const Color(0xffF4F3FE);
+}
+
+class TextColor {
+  Color black = const Color(0xff212121);
+  Color white = const Color(0xffEBEBEB);
+  Color grey = const Color(0xff818181);
+  Color lightGrey = const Color(0xffc9c8d3);
+  Color disabled = const Color(0xffc5c0db);
+}
+
+class TextColorInterface {
+  Color validation = const Color(0xff605dec);
+  Color error = const Color(0xfff93232);
+  Color success = const Color(0xff439f6e);
+}
+
+class ButtonPrimaryColor {
+  Color bgDefault = const Color(0xff605dec);
+  Color bgHover = const Color(0xfff3d3ace);
+  Color bgFocused = const Color(0xff3d3ace);
+  Color bgActive = const Color(0xff3d3ace);
+  Color bgClick = const Color(0xff23209f);
+  Color bgDisabled = const Color(0xfff4f3fe);
+
+  Color strokeFocused = const Color(0xfff845fc);
+
+  Color textDisabled = const Color(0xffc5c0db);
+  Color text = const Color(0xfffffffff);
+}
+
+class FieldsColor {
+  Color form = const Color(0xfff9f9fd);
+  Color title = const Color(0xff9d99ac);
+  Color text = const Color(0xff212121);
+  Color textDisabled =const Color(0xffa5a5a7);
+
+  Color description = const Color(0xffAAAAAA);
+
+  Color strokeHover = const Color(0xffe8e6f8);
+  Color strokeFocused = const Color(0xff605dec);
+ 
+}
